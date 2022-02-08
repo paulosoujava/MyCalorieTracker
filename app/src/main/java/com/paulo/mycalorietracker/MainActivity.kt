81058216ca437c1182c23bc8c3f3b3eeaa9a9ea3
paulosoujava <paulosoujava@gmail.com>
2022-02-08 17:31:36 -0300

    sceen welcome

--- app/src/main/java/com/paulo/mycalorietracker/MainActivity.kt
+++ app/src/main/java/com/paulo/mycalorietracker/MainActivity.kt
@@ -3,11 +3,16 @@ package com.paulo.mycalorietracker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.paulo.onboarding_presentation.welcome.WelcomeScreen
+import com.plcoding.calorytracker.ui.theme.CaloryTrackerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            CaloryTrackerTheme {
+                WelcomeScreen()
+            }
         }
     }
 }
